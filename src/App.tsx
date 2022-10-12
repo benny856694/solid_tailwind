cb4779a85d2289d11973bcebf03fc2d41a9e9b12
benny856694 <shenbinsc@gmail.com>
2022-10-12 21:47:50 +0800

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import { Component, createSignal, onCleanup } from 'solid-js';
 import Chip from './MultiSelection/Chip';
 import MultiSelection from './MultiSelection/MultiSelection';
 
-
-
 const App: Component = () => {
   const [count, setCount] = createSignal(0);
   const [x, setX] = createSignal(0);
   const [y, setY] = createSignal(0);
   const [hidden, setHidden] = createSignal(true);
-  const [selected, setSelected] = createSignal<string[]>(['Beijing', 'Chengdu']);
-  const options = ['Beijing', 'Chengdu', 'Shanghai', 'Guangzhou', 'Shenzhen', 'Hangzhou'];
+  const [selected, setSelected] = createSignal<string[]>([
+    'Beijing',
+    'Chengdu',
+  ]);
+  const options = [
+    'Beijing',
+    'Chengdu',
+    'Shanghai',
+    'Guangzhou',
+    'Shenzhen',
+    'Hangzhou',
+    'Beijing',
+    'Chengdu',
+    'Shanghai',
+    'Guangzhou',
+    'Shenzhen',
+    'Hangzhou',
+  ];
   let el: HTMLButtonElement | undefined;
 
   return (
@@ -23,48 +37,21 @@ const App: Component = () => {
         //}
       }}
     >
-      <button
-        class="p-2 bg-blue-400 text-white rounded-md mt-4 ml-4 uppercase border-2"
-        ref={el}
-        onClick={(e) => {
-          e.target.requestFullscreen()
-        }}
-      >
-        Full Screen
-      </button>
-      <p>
-        Mouse Position: X: {x()}, Y: {y()}
-      </p>
-      <div
-        
-        classList={{
-          'w-40': true,
-          'h-40': true,
-          'bg-red-500': true,
-          absolute: true,
-          'rounded-md': true,
-          'shadow-2xl': true,
-          'z-10': true,
-          'py-2': false,
-          'overflow-hidden': true,
-          hidden: hidden(),
-        }}
-        onClick={() => setHidden(true)}
-        style={{ left: x() + 'px', top: y() + 'px' }}
-      >
-        <p class="hover:bg-gray-400 hover:text-white m-2 rounded-md cursor-pointer py-1 px-2">
-          Menu1
-        </p>
-        <p class="hover:bg-gray-400 hover:text-white m-2 rounded-md cursor-pointer py-1 px-2">
-          Menu1
-        </p>
+      <div class="flex relative mt-4 gap-x-2 w-full h-16 bg-black px-2 overflow-hidden">
+        {options.map(c => (
+          <button class="text-white rounded-full px-2.5 py-1 border border-gray-600/75 bg-gray-700/75 self-center">
+            {c}
+          </button>
+        ))}
+
+        <div class="absolute top-0 right-0 bottom-0 w-8 bg-gradient-to-r from-transparent via-gray-900/50 to-gray-900"></div>
       </div>
-      <div class="flex mt-4 gap-x-2">
-        <div class="w-20 bg-red-500 h-20 grow"></div>
-        <div class="w-20 bg-red-500 h-20"></div>
-      </div>
-      <br/>
-      <MultiSelection options={options} selected={selected()} setSelected={setSelected} />
+      <br />
+      <MultiSelection
+        options={options}
+        selected={selected()}
+        setSelected={setSelected}
+      />
     </div>
   );
 };
